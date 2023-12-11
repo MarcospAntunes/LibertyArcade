b77b4f5aa10e1163e4fe52c014381d9c30bfac16
Marcospantunes <marcospantunes203@gmail.com>
2023-12-11 18:45:31 -0300

    Melhoria no responsivo

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,23 +8,22 @@ function Home(): JSX.Element {
     return(
         <>
             <Header />
-            <main id="home">
-                <section>
+            <main>
+                <section id="sectionLeft">
                     <h1>Liberty Arcade</h1>
                     <h2>Games for all</h2>
-                    
-                    <hr />
 
-                    <p>Liberty Arcade, where fun is free and the freedom to play reigns, offers a unique gaming experience as an invitation to explore a world of entertainment without financial borders.</p>
+                    <p>
+                        <hr />
+                        Liberty Arcade, where fun is free and the freedom to play reigns, offers a unique gaming experience as an invitation to explore a world of entertainment without financial borders.
+                    </p>
                     <Button text="Get Started now!" />
                 </section>
 
-                <section>
-                    <BiSolidJoystick />
-                    
+                <section id="sectionRight">
+                    <BiSolidJoystick id="joystickAnimate" />
                 </section>
-            </main>
-           
+            </main>      
         </>
     )
 }
