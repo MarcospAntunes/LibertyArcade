2a10eb96cd695b69f40ce76c320e8c9a96c2ad61
Marcospantunes <marcospantunes203@gmail.com>
2023-12-12 20:54:06 -0300

    Adicionado tipagem para jogo

--- src/interfaces/game.ts
+++ src/interfaces/game.ts
@@ -1,13 +1,40 @@
+export type gamesProps = {
+    developer?: string
+    freetogame_profile_url?: string
+    game_url?: string
+    genre?: string
+    id?: number
+    platform?: string
+    publisher?: string
+    release_date?: string
+    short_description?: string
+    thumbnail?: string
+    title?: string
+}
+
 export type gameProps = {
-    developer: string
-    freetogame_profile_url: string
-    game_url: string
-    genre: string
-    id: number
-    platform: string
-    publisher: string
-    release_date: string
-    short_description: string
-    thumbnail: string
-    title: string
+    developer?: string
+    freetogame_profile_url?: string
+    game_url?: string
+    genre?: string
+    id?: number
+    platform?: string
+    publisher?: string
+    release_date?: string
+    description?: string
+    thumbnail?: string
+    title?: string
+    minimum_system_requirements?: {
+        os?: string
+        processor?: string
+        memory?: string
+        graphics?: string
+        storage?: string
+    }
+    screenshots?: [
+        {
+            id?: number
+            image?: string
+        }
+    ]
 }
