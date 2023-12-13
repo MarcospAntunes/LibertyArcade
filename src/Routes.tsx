069fbb5699d9644539c528caf3323994b1ed490c
Marcospantunes <marcospantunes203@gmail.com>
2023-12-13 10:56:05 -0300

    Update Routes.tsx

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import Games from './pages/Games.tsx'
 import './styles/main.sass'
-import GameOverview from './pages/gameOverview.tsx'
+import GameOverview from './pages/GameOverview.tsx'
 
 function Router(): JSX.Element {
 
