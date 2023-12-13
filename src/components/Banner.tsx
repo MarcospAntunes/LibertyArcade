92a20f9d7aa022029d64aaf60895dd2bb2a17cd9
Marcospantunes <marcospantunes203@gmail.com>
2023-12-13 10:53:00 -0300

    Criado componente Banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react"
+import { fetchPhotoGame } from "../services/fetchData"
+import "../styles/components/Banner.sass"
+
+function Banner(): JSX.Element {
+    const [ gamePhoto, setGamePhoto ] = useState("")
+
+    useEffect(() => {
+        fetchPhotoGame({ setGamePhoto })
+    }, [])
+
+    const noPhoto = gamePhoto === "" ? "Error when searching for image" : ""
+    
+    return(
+        <div style={{backgroundImage: `url(${gamePhoto})`}} id="bannerPhoto">
+            <h2>{noPhoto}</h2>
+        </div>
+    )
+}
+
+
+export default Banner
