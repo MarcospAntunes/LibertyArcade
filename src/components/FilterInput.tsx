2d27c1183612d61f10a282421db546c1cc7d5734
Marcospantunes <marcospantunes203@gmail.com>
2023-12-13 11:15:55 -0300

    Melhorado design da lista

--- src/components/FilterInput.tsx
+++ src/components/FilterInput.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { MdCheckBoxOutlineBlank } from "react-icons/md";
 import { IoCheckbox } from "react-icons/io5";
 import { filterGenreFunction } from "../utils/filterGamesFunction";
+import '../styles/components/FilterInput.sass'
 
 type FilterInputProps = {
    name: string | undefined
@@ -18,10 +19,10 @@ function FilterInput({ name, value, search, setSearch }: FilterInputProps): JSX.
      filterGenreFunction({ filter, setFilter, value, search, setSearch });
    };
  
-   const checkbox = filter ? <IoCheckbox  /> : <MdCheckBoxOutlineBlank />;
+   const checkbox = filter ? <IoCheckbox /> : <MdCheckBoxOutlineBlank />;
  
    return (
-     <span onClick={handleCheckboxClick}>
+     <span onClick={handleCheckboxClick} id="filterContentContainer">
        {checkbox} {name}
      </span>
    );
