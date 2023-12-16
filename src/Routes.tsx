0b91233423ab398f680b81bb394b52382ff2c73e
Marcospantunes <marcospantunes203@gmail.com>
2023-12-15 22:58:53 -0300

    Criado pagina de login e registro

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,20 +1,34 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home.tsx'
 import Games from './pages/Games.tsx'
 import './styles/main.sass'
 import GameOverview from './pages/GameOverview.tsx'
+import Login from './pages/Login.tsx'
+import { AuthProvider } from './contexts/auth.tsx'
+import useAuth from './hooks/useAuth.tsx'
 
 function Router(): JSX.Element {
 
+  const Private = ({Item}: any): JSX.Element => {
+    const { signed }: any = useAuth();
+  
+    return signed > 0 ? <Item /> : <Login />;
+  }
+
   return (
     <>
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />}/>
           <Route path='/games' element={<Games />}/>
           <Route path="/game/:title" element={<GameOverview />} />
         </Routes>
       </BrowserRouter>
+    </AuthProvider>
+      
     </>
   )
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Link, useNavigate } from "react-router-dom";
+import Form from '../components/Form'
+import InputForm from "../components/inputForm";
+import { useState } from "react";
+import useAuth from "../hooks/useAuth";
+import { handleLogin } from "../utils/validFom";
+import '../styles/components/Login.sass'
+
+function Login(): JSX.Element {
+    const { login }: any = useAuth()
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    return(
+        <main id="mainLogin">
+            <section id="formLoginContainer">
+                <h1>Login</h1>
+                <Form>
+                    <InputForm
+                        type="email"
+                        placeholder="Type your e-mail"
+                        value={email}
+                        required
+                        onChange={(e) => [setEmail(e.target.value), setError("")]}
+                    />
+                    <InputForm
+                        type="password"
+                        placeholder="Type your password"
+                        value={password}
+                        required
+                        onChange={(e) => [setPassword(e.target.value), setError("")]}
+                    />
+                    <InputForm
+                        type="submit"
+                        value="Login"
+                        onClick={(e) => handleLogin({e, setError, email, password, login, navigate})}
+                    />
+                </Form>
+                <p>{error}</p>
+                <p>Don't have an account? <Link to={"/register"}>Click here</Link> and create your account for free!</p>
+            </section>
+        </main>
+    )
+}
+
+export default Login;
