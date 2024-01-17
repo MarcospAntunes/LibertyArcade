fb018a2265e8b158c4990b52229a05281cbe8b94
Marcospantunes <marcospantunes203@gmail.com>
2024-01-17 14:38:50 -0300

    Melhorado descrição do erro quando não há jogos

--- src/components/GamesList.tsx
+++ src/components/GamesList.tsx
@@ -2,7 +2,7 @@
 import '../styles/components/GamesList.sass'
 
 function GamesList({children}: any): JSX.Element {
-    const element = children.length !== 0 ? children : <li>Not Found</li>;
+    const element = children.length !== 0 ? children : <li>Unable to load games or no games found</li>;
     return(
         <ul id='gameList'>
             {element}
